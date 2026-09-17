a5ea9d535cb0c08e038560b9ab065cffb7c519c0
ThaiBinh <binhnguyen.cnit@gmail.com>
2026-09-18 00:18:22 +0700

    feat: design login screnn ui

---
+++ src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>ĐĂNG NHẬP</Text>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={pass}
+          onChangeText={setPass}
+          secureTextEntry
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>ĐĂNG NHẬP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Chưa có tài khoản? Đăng ký ngay</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    // borderWidth: 1,
+  },
+  title: {
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignSelf: "center",
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    color: "#ca5757",
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 12,
+  },
+  button: {
+    borderWidth: 1,
+    alignItems: "center",
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 100,
+    marginTop: 15,
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#f5b1b1",
+  },
+});
+
+export default LoginScreen;
