841c273048ee72edeb60ed53263613246b746606
ThaiBinh <binhnguyen.cnit@gmail.com>
2026-09-18 00:40:44 +0700

    feat: design register screnn ui with validation

---
+++ src/screens/auth/RegisterScreen.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+
+const RegisterScreen = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = () => {
+    if (!email || !pass || !displayName) {
+      Alert.alert("Thông báo", "Email và Password không được để trống");
+      return;
+    }
+    if (pass != confirmPassword) {
+      Alert.alert("Thông báo", "Password không trùng khớp");
+      return;
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>ĐĂNG KÝ</Text>
+        <TextInput
+          placeholder="Username"
+          value={displayName}
+          onChangeText={setDisplayName}
+          autoCapitalize="none"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={pass}
+          onChangeText={setPass}
+          secureTextEntry
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleRegister}>
+          <Text>ĐĂNG KÝ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Đã có tài khoản? Đăng nhập ngay</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    // borderWidth: 1,
+  },
+  title: {
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignSelf: "center",
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    color: "#ca5757",
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 12,
+  },
+  button: {
+    borderWidth: 1,
+    alignItems: "center",
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 100,
+    marginTop: 15,
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: "#f5b1b1",
+  },
+});
+
+export default RegisterScreen;
